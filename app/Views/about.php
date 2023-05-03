4e6b24c35fd9d8354505d60456e867bcd0172d5c
np <nerpoc42@gmail.com>
2023-05-03 14:16:14 +0300

    Removed triggers from dump file, updated about

--- app/Views/about.php
+++ app/Views/about.php
@@ -71,48 +71,6 @@ create index order_state
 create index user_id
     on orders (user_id, product_id);
 
-create definer = np@localhost trigger delete_orders_update_money
-    before delete
-    on orders
-    for each row
-    UPDATE users u SET u.money = u.money + OLD.product_amount * OLD.product_cost WHERE u.id = OLD.user_id;
-
-create definer = np@localhost trigger delete_orders_update_products
-    before delete
-    on orders
-    for each row
-    UPDATE products p SET p.amount = p.amount + OLD.product_amount WHERE p.id = OLD.product_id;
-
-create definer = np@localhost trigger insert_orders_update_money
-    before insert
-    on orders
-    for each row
-    UPDATE users u SET u.money = u.money - NEW.product_amount * NEW.product_cost WHERE u.id = NEW.user_id;
-
-create definer = np@localhost trigger insert_orders_update_products
-    before insert
-    on orders
-    for each row
-    UPDATE products p SET p.amount = p.amount - NEW.product_amount WHERE p.id = NEW.product_id;
-
-create definer = np@localhost trigger update_orders_update_money
-    before update
-    on orders
-    for each row
-BEGIN
-    UPDATE users u SET u.money = u.money + OLD.product_amount * OLD.product_cost WHERE u.id = OLD.user_id;
-    UPDATE users u SET u.money = u.money - NEW.product_amount * NEW.product_cost WHERE u.id = NEW.user_id;
-end;
-
-create definer = np@localhost trigger update_orders_update_products
-    before update
-    on orders
-    for each row
-BEGIN
-    UPDATE products p SET p.amount = p.amount + OLD.product_amount WHERE p.id = OLD.product_id;
-    UPDATE products p SET p.amount = p.amount - NEW.product_amount WHERE p.id = NEW.product_id;
-END;
-
 create table shopping_carts
 (
     id             int auto_increment
